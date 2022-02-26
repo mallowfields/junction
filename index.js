c65154d741afb06eed1aad1ada7142a94eb5625d
Kendell Joseph <kendell@mallowfields.com>
2022-02-25 23:29:18 -0500

    Adds docs

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // # Junction
 // Human Workflow Marketplace
 // ## Entities
-// - [Map](/entities/Map.html)
+// - [Card](entities/Card.html)
+// - [Dashboard](entities/Dashboard.html)
+// - [Deck](entities/Deck.html)
+// - [Dispatch](entities/Dispatch.html)
+// - [Extension](entities/Extension.html)
+// - [Marketplace](entities/Marketplace.html)
+// - [Security](entities/Security.html)
+// - [Site](entities/Site.html)
+// - [Terms](entities/Terms.html)
+// - [Workflow](entities/Workflow.html)
 console.log('Junction - Human Workflow Marketplace');
 
 
