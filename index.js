2c17ed689b677c99085f280533ed72c4e2aabc33
Kendell Joseph <kendell@mallowfields.com>
2022-02-25 20:15:41 -0500

    WIP - adds design reference

--- index.js
+++ index.js
@@ -3,3 +3,29 @@
 // ## Entities
 // - [Map](/entities/Map.html)
 console.log('Junction - Human Workflow Marketplace');
+
+
+// TEMP: REPL Design Reference Shape
+
+// const repl = require('repl');
+
+// function myEval(cmd, context, filename, callback) {
+//     let result;
+//     try {
+//         result = vm.runInThisContext(cmd);
+//     } catch (e) {
+//         if (isRecoverableError(e)) {
+//             return callback(new repl.Recoverable(e));
+//         }
+//     }
+//     callback(null, cmd);
+//   }
+  
+//   function isRecoverableError(error) {
+//     if (error.name === 'SyntaxError') {
+//       return /^(Unexpected end of input|Unexpected token)/.test(error.message);
+//     }
+//     return false;
+// }
+
+// repl.start({ prompt: 'junction> ', eval: myEval });
