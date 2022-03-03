c1d7057f9e5b5f459df0d0778e2047b6df3b4045
Kendell Joseph <kendell@mallowfields.com>
2022-03-03 11:51:54 -0500

    Adds loading

---
+++ src/junction-shell-ui/src/components/Loading.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="loading-container">
+    <v-progress-circular
+      :indeterminate="true"
+      :size="inline ? 32 : 128"
+      :width="inline ? 4 : 8"
+      color="main"
+    ></v-progress-circular>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    inline: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
