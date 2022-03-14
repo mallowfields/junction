1de3ca2007c24509f44b5eff1d06ac7f4f946790
Kendell Joseph <kendell@mallowfields.com>
2022-03-14 15:19:06 -0400

    Linting

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ app.use(history())
 app.use(express.static(`${__dirname}/dist`))
 
 const port = process.env.PORT || 8080
-const hostname = '127.0.0.1'
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}/`)
 })
