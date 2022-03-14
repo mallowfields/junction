4677dd1c4e8d7a9ea819eceaf23eaf262119053f
Kendell Joseph <kendell@mallowfields.com>
2022-03-14 15:07:30 -0400

    Linting

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+require('dotenv').config()
 const express = require('express')
 const history = require('connect-history-api-fallback')
-
 const app = express()
 app.use(history())
 app.use(express.static(`${__dirname}/dist`))
